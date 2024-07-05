12bb4756f43126ad2af9cb45fd1302a4ab76347c
Nomera67 <94490777+Nomera67@users.noreply.github.com>
2024-07-05 23:19:05 +0200

    Add media queries

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,70 @@
 import './Header.scss';
 
+import { useState, useEffect } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 
 
 function Header() {
 
+    const [isNavVisible, setIsNavVisible] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavVisible(!isNavVisible);
+    };
+
     const location = useLocation();
+
+    useEffect(() => {
+        const primaryNav = document.getElementById('primary-navigation');
+        const items = Array.from(document.querySelectorAll('.nav__item'));
+    
+        const handleMouseOver = (index: number) => {
+          if (primaryNav) {
+            primaryNav.setAttribute('data-active-index', index.toString());
+          }
+        };
+    
+        items.forEach((item, index) => {
+          item.addEventListener('mouseover', () => handleMouseOver(index));
+        });
+    
+        return () => {
+          items.forEach((item) => {
+            item.removeEventListener('mouseover', () => handleMouseOver);
+          });
+        };
+      }, [isNavVisible]); 
 
     return (
         <header className='header'>
-            <div className="header__logo">
+            <div className="header__logo" onClick={() => window.location.href = '/'}>
                 <span>YR</span>
             </div>
-            <nav className='header__nav'>
-                <NavLink to="/home" className={location.pathname === '/home' ? 'active' : ''}>Accueil</NavLink>
-                <NavLink to="/about" className={location.pathname === '/about' ? 'active' : ''}>À propos</NavLink>
-                <NavLink to="/contact" className={location.pathname === '/contact' ? 'active' : ''}>Contact</NavLink>
+            <button 
+                id="toggleButton" 
+                className={`mobileNavToggle ${isNavVisible ? 'expanded' : ''}`} 
+                aria-controls="primary-navigation" 
+                aria-expanded={isNavVisible} 
+                onClick={toggleNav}
+            >
+                <span className="toggle__line"></span>
+                <span className="toggle__line"></span>
+                <span className="toggle__line"></span>
+            </button>
+            <nav className={`header__nav ${isNavVisible ? 'visible' : ''}`} id="navBar">
+                <ul id="primary-navigation" className="nav__list" data-visible={isNavVisible}>
+                    <li className="nav__item">
+                        <NavLink to="/home" className={location.pathname === '/home' ? 'active' : ''}>Accueil</NavLink>
+                    </li>
+                    <li className="nav__item">
+                        <NavLink to="/about" className={location.pathname === '/about' ? 'active' : ''}>À propos</NavLink>
+                    </li>
+                    <li className="nav__item">
+                        <NavLink to="/contact" className={location.pathname === '/contact' ? 'active' : ''}>Contact</NavLink>
+                    </li>
+                    <div className="nav__list__points"></div>
+                    <div className="nav__list__background" id="background__menu"></div>
+                </ul>
             </nav>
         </header>
     )
