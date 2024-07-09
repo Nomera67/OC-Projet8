c24e996320812441b2e9001661ee1771f5819009
Nomera67 <y.rechtenstein@gmail.com>
2024-07-09 20:18:27 +0200

    fix SEO

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,10 @@
 import './Header.scss';
-
 import { useState, useEffect } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 
-
 function Header() {
-
     const [isNavVisible, setIsNavVisible] = useState(false);
+    const [activeIndex, setActiveIndex] = useState(0);
 
     const toggleNav = () => {
         setIsNavVisible(!isNavVisible);
@@ -15,43 +13,57 @@ function Header() {
     const location = useLocation();
 
     useEffect(() => {
-        const primaryNav = document.getElementById('primary-navigation');
+        switch (location.pathname) {
+            case '/home':
+                setActiveIndex(0);
+                break;
+            case '/about':
+                setActiveIndex(1);
+                break;
+            case '/contact':
+                setActiveIndex(2);
+                break;
+            default:
+                setActiveIndex(0);
+        }
+    }, [location.pathname]);
+
+    useEffect(() => {
         const items = Array.from(document.querySelectorAll('.nav__item'));
-    
-        const handleMouseOver = (index: number) => {
-          if (primaryNav) {
-            primaryNav.setAttribute('data-active-index', index.toString());
-          }
+
+        const handleMouseOver = (index: any) => {
+            setActiveIndex(index);
         };
-    
+
         items.forEach((item, index) => {
-          item.addEventListener('mouseover', () => handleMouseOver(index));
+            item.addEventListener('mouseover', () => handleMouseOver(index));
         });
-    
+
         return () => {
-          items.forEach((item) => {
-            item.removeEventListener('mouseover', () => handleMouseOver);
-          });
+            items.forEach((item) => {
+                item.removeEventListener('mouseover', () => handleMouseOver);
+            });
         };
-      }, [isNavVisible]); 
+    }, [isNavVisible]);
 
     return (
         <header className='header'>
             <div className="header__logo" onClick={() => window.location.href = '/'}>
                 <span>YR</span>
             </div>
-            <button 
-                id="toggleButton" 
-                className={`mobileNavToggle ${isNavVisible ? 'expanded' : ''}`} 
-                aria-controls="primary-navigation" 
-                aria-expanded={isNavVisible} 
+            <button
+                id="toggleButton"
+                className={`mobileNavToggle ${isNavVisible ? 'expanded' : ''}`}
+                aria-label="Navigation Menu"
+                aria-controls="primary-navigation"
+                aria-expanded={isNavVisible}
                 onClick={toggleNav}
             >
                 <span className="toggle__line"></span>
                 <span className="toggle__line"></span>
                 <span className="toggle__line"></span>
             </button>
-            <nav className={`header__nav ${isNavVisible ? 'visible' : ''}`} id="navBar">
+            <nav className={`header__nav ${isNavVisible ? 'visible' : ''}`} id="navBar" data-active-index={activeIndex}>
                 <ul id="primary-navigation" className="nav__list" data-visible={isNavVisible}>
                     <li className="nav__item">
                         <NavLink to="/home" className={location.pathname === '/home' ? 'active' : ''} aria-label={`Lien vers l'accueil`}>Accueil</NavLink>
@@ -62,12 +74,12 @@ function Header() {
                     <li className="nav__item">
                         <NavLink to="/contact" className={location.pathname === '/contact' ? 'active' : ''} aria-label={`Lien vers la section de contact`}>Contact</NavLink>
                     </li>
-                    <div className="nav__list__points"></div>
-                    <div className="nav__list__background" id="background__menu"></div>
                 </ul>
+                <div className="nav__list__points"></div>
+                <div className="nav__list__background" id="background__menu"></div>
             </nav>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
